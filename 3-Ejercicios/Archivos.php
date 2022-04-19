1de2f86e5921caf447e91b2b9603b57698e5bae7
JuanMaChico <juanmanuelchico@hotmail.com>
2022-04-19 18:54:11 -0300

    Cambio de nombre

---
+++ 3-Ejercicios/Archivos.php
@@ -0,0 +1,42 @@
+
+<?php
+
+//Apertura del archivo de texto
+$archivo = fopen("archivo.txt", "r");
+
+//Validamos que se abri correctamente
+if ($archivo) {
+    echo "<br> El archivo se abrio correctamente";
+}
+
+//Impresion por pantalla
+echo "<br> ---> Procesamos el archivo<br>";
+
+while (!feof($archivo)) {
+    
+    echo "<br>Entra";
+    echo fgets($archivo) . "<br>";
+
+}
+echo "<br> ---> fgets()imprime hasta llegar al final del archivo <br>";
+
+// Asi leemos el archivo
+//echo "<br><br>Lectura del archivo<br>--->" . fread($archivo, filesize("archivo.txt")) . "<br><br>";
+
+while (!feof($archivo)) {
+
+    echo "Se llego el fin del archivo<br> Por eso no se imprime nada";
+    echo fgets($archivo) . "<br>";
+
+}
+
+echo copy("archivo.txt", "copia.txt");
+
+// echo unlink("copia.txt");
+
+//Cerramos el archivo
+if (fclose($archivo)) {
+    echo "<br> El archivo se cerro correctamente";
+}
+
+?>
---
+++ 3-Ejercicios/MetodosPostGet.php
@@ -0,0 +1,12 @@
+<?php
+
+$nombre = $_POST["nombre"];
+
+// echo $_GET.$_POST["nombre"];
+
+echo "<br>".$nombre. "<br>";
+
+var_dump($_REQUEST);
+
+
+?>
