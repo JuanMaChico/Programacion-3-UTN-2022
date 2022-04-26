4c5c3a44c1f333c662ab9bdc6003018a109bbeeb
JuanMaChico <juanmanuelchico@hotmail.com>
2022-04-26 00:55:55 -0300

    Ejercicios Parte 4

--- EjerciciosPHP-2/Parte-5.php
+++ EjerciciosPHP-2/Parte-5.php
@@ -1,47 +1,47 @@
 <?php
 
 //Importamos la clase auto
-require_once ("Ejercicio-4.php");
+require_once ("Parte-4.php");
 
 /*
-        Aplicación No 18 (Auto - Garage)
-        Crear la clase Garage que posea como atributos privados:
+    Aplicación No 18 (Auto - Garage)
+    Crear la clase Garage que posea como atributos privados:
 
-        _razonSocial (String)
-        _precioPorHora (Double)
-        _autos (Autos[], reutilizar la clase Auto del ejercicio anterior)
+    _razonSocial (String)
+    _precioPorHora (Double)
+    _autos (Autos[], reutilizar la clase Auto del ejercicio anterior)
 
-        Realizar un constructor capaz de poder instanciar objetos pasándole como parámetros:
+    Realizar un constructor capaz de poder instanciar objetos pasándole como parámetros:
 
-        i. La razón social.
-        ii. La razón social, y el precio por hora.
+    i. La razón social.
+    ii. La razón social, y el precio por hora.
 
-        -Realizar un método de instancia llamado “MostrarGarage”, que no recibirá parámetros y
-        que mostrará todos los atributos del objeto.
+    -Realizar un método de instancia llamado “MostrarGarage”, que no recibirá parámetros y
+    que mostrará todos los atributos del objeto.
 
-        -Crear el método de instancia “Equals” que permita comparar al objeto de tipo Garaje con un
-        objeto de tipo Auto. Sólo devolverá TRUE si el auto está en el garaje.
+    -Crear el método de instancia “Equals” que permita comparar al objeto de tipo Garaje con un
+    objeto de tipo Auto. Sólo devolverá TRUE si el auto está en el garaje.
 
-        -Crear el método de instancia “Add” para que permita sumar un objeto “Auto” al “Garage”
-        (sólo si el auto no está en el garaje, de lo contrario informarlo).
+    -Crear el método de instancia “Add” para que permita sumar un objeto “Auto” al “Garage”
+    (sólo si el auto no está en el garaje, de lo contrario informarlo).
 
-        Ejemplo: $miGarage->Add($autoUno);
+    Ejemplo: $miGarage->Add($autoUno);
 
-        -Crear el método de instancia “Remove” para que permita quitar un objeto “Auto” del
-        “Garage” (sólo si el auto está en el garaje, de lo contrario informarlo).
+    -Crear el método de instancia “Remove” para que permita quitar un objeto “Auto” del
+    “Garage” (sólo si el auto está en el garaje, de lo contrario informarlo).
 
-        Ejemplo: $miGarage->Remove($autoUno);
+    Ejemplo: $miGarage->Remove($autoUno);
 
-        -En testGarage.php, crear autos y un garage. Probar el buen funcionamiento de todos los
-        métodos.
+    -En testGarage.php, crear autos y un garage. Probar el buen funcionamiento de todos los
+    métodos.
 
  */
 
 class Garage
 {
 
-    private $_razonSocial;
-    private $_preciosPorHora;
+    private String $_razonSocial;
+    private Float $_preciosPorHora;
     private $_autos = array();
 
     public function __construct($razonSocial, $precioPorHora = 0)
@@ -51,16 +51,21 @@ class Garage
     }
 
     public function MostrarGarage(){
+        echo "<br>Informacion del Garage<br>";
         echo "Razon Social: ".$this->_razonSocial."<br>";
-        echo "Precio por hora: ".$this->_preciosPorHora."<br>";
-        echo "Autos: <br>";
-        foreach ($this->_autos as $auto){
-            $auto->MostrarAuto();
+        echo "Precio por hora: $".$this->_preciosPorHora."<br>";
+        if ( count($this->_autos) > 0 ){
+            echo "Lista de Autos: <br>";
+            foreach ($this->_autos as $auto){
+               echo "<br>".Auto::MostrarAuto($auto)."<br>";
+            }
+        }else{
+            echo "<br> No hay autos en ".$this->_razonSocial."<br>";
         }
     }
 
     public function Equals($auto){
-        foreach ($_autos as $autoGarage){
+        foreach ($this->_autos as $autoGarage){
             if($autoGarage->Equals($auto)){
                 return true;
             }
@@ -70,7 +75,7 @@ class Garage
 
     public function Add($auto){
         if(!$this->Equals($auto)){
-            array_push($_autos, $auto);
+            array_push($this->_autos, $auto);
         }else{
             return "El auto ya esta en el garage";
         }
@@ -78,9 +83,9 @@ class Garage
 
     public function Remover($auto){
         if($this->Equals($auto)){
-            foreach ($_autos as $key => $value){
+            foreach ($this->_autos as $key => $value){
                 if($value->Equals($auto)){
-                    unset($_autos[$key]);
+                    unset($this->_autos[$key]);
                 }
             }
         }else{
--- EjerciciosPHP-2/TestGarage.php
+++ EjerciciosPHP-2/TestGarage.php
@@ -2,20 +2,48 @@
 //   -En testGarage.php, crear autos y un garage. Probar el buen funcionamiento de todos los
 //      métodos.
 
-require_once("Ejercicio-5.php");
+require_once("Parte-5.php");
+
+echo "<br> Ejercicio Garage <br>";
+
+$garage = new Garage("Mi Cochera", 10);
+
+$garage->MostrarGarage();
+
+$auto1 = new Auto("Ford", "Rojo",(Float)20000);
+$auto2 = new Auto("Ford", "Rojo",(Float)30000);
+$auto3 = new Auto("VW", "Negro",(Float)40000, new DateTime());
+$auto4 = new Auto("VW", "Negro",(Float)40000, new DateTime());
+$auto5 = new Auto("Nissan", "Blanco",(Float)740000, new DateTime());
+$auto6 = new Auto("Ferrari", "Amarillo",(Float)40000, new DateTime());
+$auto7 = new Auto("Fiat", "Naranja",(Float)40000, new DateTime());
+
+echo "<br>";
+echo $garage->Add($auto1);
+echo "<br>";
+echo $garage->Add($auto2);
+echo "<br>";
+echo $garage->Add($auto3);
+echo "<br>";
+echo $garage->Add($auto4);
+echo "<br>";
+echo $garage->Add($auto5);
+echo "<br>";
+echo $garage->Add($auto6);
+echo "<br>";
+echo $garage->Add($auto7);
+echo "<br>";
+
+$garage->MostrarGarage();
+
+$garage->Remover($auto1);
+
+$garage->MostrarGarage();
 
 
-$estacionamiento = new Garage("Estacionamiento", 10);
 
-// $autoTest = new Auto("Ford", "Rojo", 2500);
-// $autoTest1 = new Auto("Ford", "Rojo", 34000);
-// $autoTest2 = new Auto("Fiat", "Verde", 20030, "2018-03-01");
-// $autoTest3 = new Auto("VW", "Negro", 13454);
-// $autoTest4 = new Auto("VW", "Negro", 50000);
 
-// $estacionamiento->Add($autoTest);
 
-$estacionamiento->MostrarGarage();
 
 
 
